cd7a2b539175ea504e75be1d3ef46a1284df4731
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2022-09-27 08:41:24 +0200

    remove unneeded headers content-security-policy and x-xss-protection

--- hooks/preHandler.hook.js
+++ hooks/preHandler.hook.js
@@ -6,10 +6,10 @@
  * @param {*} next 
  */
 module.exports = function (req, reply, next) {
-  reply.header('Content-Security-Policy', `default-src 'none'`)
+  // reply.header('Content-Security-Policy', `default-src 'none'`)
   reply.header('X-Content-Type-Options', `nosniff`)
   reply.header('X-Frame-Options', `DENY`)
-  reply.header('X-Xss-Protection', `1; mode=block`)
+  // reply.header('X-Xss-Protection', `1; mode=block`)
   reply.header('Referrer-Policy', `no-referrer`)
   reply.header('Permissions-Policy', `fullscreen=*`)
   reply.header('Strict-Transport-Security', `max-age=15552000; preload`)
