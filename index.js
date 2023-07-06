0f3fd06b3090954c582fd5ce06b5b5ac4ace6d33
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2023-07-06 16:41:28 +0200

    release updated export version, move responses to seperate folder, revert export in main index file

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function addDefaultRequestHooks(fastify) {
  * @export
  * @class Fastify
  */
-class Fastify {
+module.exports = class Fastify {
   /** @type {Function} */
   authPreHandler
 
@@ -214,78 +214,6 @@ class Fastify {
       process.exit(1)
     }
   }
-}
-
-// export interface IBaseAPIResponse {
-//   status: 'error' | 'success' | 'fail'
-//   code?: number // HTTP status code
-//   message?: string // Required when status is 'error'
-//   data?: { [key: string]: any } | any // Required if status is 'success' or 'fail', data should never be an array
-//   executionTime?: number // Optional: Execution time in milliseconds
-// }
-
-/**
- * Return a sucessful response
- * @param {fastify.FastifyReply} reply
- * @param {any | null} data
- * @param {number | undefined} code
- * @param {number | undefined} executionTime
- * @returns {fastify.FastifyReply}
- */
-const success = function (reply, data, code = 200, executionTime = undefined) {
-  return reply
-    .code(code)
-    .send({
-      status: 'success',
-      code,
-      data,
-      executionTime
-    })
-}
-
-/**
- * Return a failed response
- * @param {fastify.FastifyReply} reply
- * @param {any | null} data
- * @param {number | undefined} code
- * @param {number | undefined} executionTime
- * @returns {fastify.FastifyReply}
- */
-const fail = function (reply, data, code = 400, data = {}, executionTime = undefined) {
-  return reply
-    .code(code)
-    .send({
-      status: 'fail',
-      code,
-      data,
-      executionTime
-    })
-}
-
-/**
- * Return a error response
- * @param {fastify.FastifyReply} reply
- * @param {string} message
- * @param {number | undefined} code
- * @param {number | undefined} executionTime
- * @returns {fastify.FastifyReply}
- */
-const error = function (reply, message, executionTime = undefined) {
-  return reply
-    .code(code)
-    .send({
-      status: 'error',
-      code,
-      message,
-      executionTime
-    })
-}
-
-module.exports = {
-  Fastify: Fastify,
-  success,
-  fail,
-  error
 }
 
 /*
