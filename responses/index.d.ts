7ff6b4e0308743c3703f31e8708567be57895f06
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2023-08-10 09:41:09 +0200

    allow data to be undefined

--- responses/index.d.ts
+++ responses/index.d.ts
@@ -1,5 +1,5 @@
 import { FastifyReply } from 'fastify'
 
-export function success(reply: FastifyReply, data: any, code: number = 200, executionTime?: number): FastifyReply
+export function success(reply: FastifyReply, data?: any, code: number = 200, executionTime?: number): FastifyReply
 export function fail(reply: FastifyReply, data: any, code: number = 400, executionTime?: number): FastifyReply
 export function error(reply: FastifyReply, message: string, code: number = 500, executionTime?: number): FastifyReply
