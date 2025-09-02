0b2ec7549f2cedad2062ee4b48785e966bd4e5ce
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2025-09-02 09:42:07 +0200

    testing new child logger for reqId

--- plugins/request-id.js
+++ plugins/request-id.js
@@ -7,27 +7,31 @@ let requestId = 0
 module.exports = fastifyPlugin(request_id)
 
 /**
- * 
- * @param {import ('fastify').FastifyInstance} fastify 
+ *
+ * @param {import ('fastify').FastifyInstance} fastify
  */
 async function request_id(fastify) {
   fastify.log.debug('adding plugin request_id')
 
-  fastify.addHook('onSend', set_header)
+  fastify.addHook('onRequest', set_header)
 }
 
 /**
  * set request-id header
- * @param {import ('fastify').FastifyRequest} request 
- * @param {import ('fastify').FastifyReply} reply 
+ * @param {import ('fastify').FastifyRequest} request
+ * @param {import ('fastify').FastifyReply} reply
  * @param {function} done
  */
 async function set_header(request, reply) {
-  reply.header('request-id', request.id)
+  let reqId = generateRequestId()
+  reply.header('request-id', reqId)
+  request.log = request.log.child({ http: { request: { id: reqId } } })
 }
 
-module.exports.generate_request_id = function () {
+function generateRequestId() {
   if (requestId >= maxRequestId)
     process.exit(13)
   return hostname + ('0000000000' + (++requestId).toString(36)).slice(-10)
 }
+
+module.exports.generate_request_id = generateRequestId
